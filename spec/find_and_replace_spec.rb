46ff20df07df33d65de5d9cd26e7c8fa3789015b
Andrew Giang <giang184@gmail.com>
2021-07-14 14:31:26 -0700

    add some files
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>
    co-authored-by: Kristen Hopper <hopperdavis@gmail.com>

---
+++ spec/find_and_replace_spec.rb
@@ -0,0 +1,18 @@
+require('rspec')
+require('find_and_replace')
+require('pry')
+
+describe('#title_case') do
+  it("search for world and replace with universe") do
+    expect("Hello world".find_and_replace("world", "universe")).to(eq("Hello universe"))
+  end
+
+  it("___") do
+    expect("I am walking to the cathedral".find_and_replace("cat", "dog")).to(eq("I am walking to the doghedral"))
+  end
+
+  it("search for world and replace with universe") do
+    expect("I am walking my cat to the cathedral".find_and_replace("cat", "dog")).to(eq("I am walking my dog to the doghedral"))
+  end
+  
+end
